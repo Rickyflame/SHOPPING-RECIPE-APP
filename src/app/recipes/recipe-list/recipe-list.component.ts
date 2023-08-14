4c4a18973e3489c3426182541a916d5b4e92ee68
Kimumu Ricky <62835201+Rickyflame@users.noreply.github.com>
2023-08-14 10:24:39 +0300

    updated parts of the recipe

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a path', 'https://cdn.pixaby.com')
+  ];
+
+  constructor() {}
+
+  ngOninit() {
+
+  }
+}
