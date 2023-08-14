3491ca78b705db9c23a1600fcf801557c5d0e1da
Kimumu Ricky <62835201+Rickyflame@users.noreply.github.com>
2023-08-14 13:39:20 +0300

    updated inputs

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a path', 'https://cdn.pixaby.com')
+    new Recipe('A test recipe', 'This is simply a path', 'https://w7.pngwing.com/pngs/426/730/png-transparent-logo-yellow-font-recipe-logo-art-yellow.png'),
+    new Recipe('Another test recipe', 'This is simply a path', 'https://w7.pngwing.com/pngs/873/295/png-transparent-speech-language-pathology-android-recipe-android.png')
+
   ];
 
   constructor() {}
 
   ngOninit() {
 
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -15,5 +15,9 @@ export class ShoppingListComponent  implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+  
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+  }
 
 }
